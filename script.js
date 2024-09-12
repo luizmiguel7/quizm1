ff42b52ea2c7b3d72f7a5ac6153c6ffe26c8918f
luizmiguel7 <luizmiguel.zx7@gmail.com>
2024-09-12 18:17:19 -0300

    commit um

---
+++ script.js
@@ -0,0 +1,37 @@
+//* function verificarCandidatura(nomeUsuario, Idadefuncionario, tempoTrabalho){
+//* if(Idadefuncionario >= 25 && tempoTrabalho > 2 ){
+//* return "Funcionario aprovado para inscrição"
+//* }else{
+//*    return "Funcionario reprovado para inscrição"
+//* }
+
+//* }
+
+//* var nome = prompt('Digite o seu nome !')
+//* var idade = prompt("digite a sua idade") 
+//* var tempo = prompt('digite quantos anos tem na empresa')
+//* console.log(verificarCandidatura(nome,idade, tempo))
+
+
+//-------------------------------------------------------
+
+//* TIPOS DE DADOS ?
+//* number | boolean | string
+
+//* tipos de variaveis?
+//* var | let | const
+
+//* VAR -> GLOBAL significa que suas variaveis são acessiveis e podem ser alteradas
+//* no seu escopo/ blobo de codigo global
+
+//*var nome = "Beatriz";
+
+//* function showName(){
+//* console.log(nome)
+//*  }
+
+//* showName()
+//*
+//* LET -> Escopo LOCAL 
+//* essa variavel 
+
